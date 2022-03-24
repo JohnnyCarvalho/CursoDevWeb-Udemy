f484dcee1d93ec5f83167d0228fbb9ee66f5ade2
JohnnyCarvalhoDev <johnnycarvalhodev@gmail.com>
2022-03-24 10:38:16 -0300

    ex 24/03

--- FUNDAMENTOS/005match.js
+++ FUNDAMENTOS/005match.js
@@ -1,8 +1,9 @@
 /**
- * Nesse exemplo vamos calcular a área de uma circunferência com o método Match
+ * Nesse exemplo vamos calcular a área de uma circunferência com o objeto Match
  */
 
 const raio  = 5.1;
 const area = Math.PI * Math.pow(raio, 2);
 
 console.log(area.toFixed(2));
+console.log(typeof Math);
