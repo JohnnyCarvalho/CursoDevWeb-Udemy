9b35bf4e593b47f290774d1c33bd14f53163003b
JohnnyCarvalhoDev <johnnycarvalhodev@gmail.com>
2022-03-24 09:35:11 -0300

    teste commit

---
+++ FUNDAMENTOS/organizacao.js
@@ -0,0 +1,16 @@
+/**
+ * Java Script é organizado através de sentenças de código e blocos de códigos.
+ * Vejamos exemplos a seguir:
+ */
+
+console.log('Sentença de código'); // não é necessário ter ; no final do código em JS
+
+{//bloco 1
+    console.log('Bloco de código...');
+    {// bloco 2
+        console.log('Hello ...');
+        console.log('Word!!');
+        console.log("My first project JS!");
+        console.log("Commit Test");
+    }
+}
